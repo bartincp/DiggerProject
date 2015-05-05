9dbe8a607f411bb4fc6360cc391c6b007f3e90f9
rolloutwo <jenkintk@rose-hulman.edu>
2015-05-05 10:04:53 -0400

    Initial emerald class changes

--- src/Emerald.java
+++ src/Emerald.java
@@ -1,4 +1,20 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
-public class Emerald {
 
+public class Emerald extends ImageIcon{
+	
+	private Icon emeraldicon;
+	private boolean emeraldstate;
+	
+	public Emerald(){
+		emeraldicon = new ImageIcon(Image );
+		emeraldstate = true;
+	}
+
+	public void collect()
+	{
+		emeraldstate = false;
+		emeraldicon = null;
+	}
 }
