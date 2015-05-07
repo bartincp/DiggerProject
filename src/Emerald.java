712c7d11d32c401acb31e7210230de84b7049cf3
rolloutwo <jenkintk@rose-hulman.edu>
2015-05-06 22:19:45 -0400

    Player and Emerald updates
    Added position methods and updaters for the emerald and player classes.

--- src/Emerald.java
+++ src/Emerald.java
@@ -6,14 +6,19 @@ public class Emerald extends ImageIcon{
 	
 	private Icon emeraldicon;
 	private boolean emeraldstate;
+	private int xposition, yposition;
 	
-	public Emerald(){
-		emeraldicon = new ImageIcon("desktop/Emerald");
-		emeraldstate = true;
+	public Emerald(int xaxis, int yaxis){
+		this.emeraldicon = new ImageIcon("desktop/Emerald");
+		this.emeraldstate = true;
+		this.xposition = xaxis;
+		this.yposition = yaxis;
 	}
 
 	public void collect(){
 		emeraldstate = false;
 		emeraldicon = null;
+		xposition = -1;
+		yposition = -1;
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,51 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Player {
+import javax.swing.Icon;
+import javax.swing.KeyStroke;
 
+
+public class Player implements ActionListener{
+	
+	private Icon playericon;
+	private boolean playerstate;
+	private int xposition, yposition;
+	private static final int xrespawn, yrespawn, gridwidth, gridheight;
+	
+	public Player(){
+		
+	}
+	
+	public void death(){
+		playerstate = false;
+	}
+	
+	public void respawn(){
+		xposition = xrespawn;
+		yposition = yrespawn;
+	}
+
+	public void moveRight(){
+		if(xposition>=0 && xposition<gridwidth)
+			xposition++;
+	}
+	
+	public void moveLeft(){
+		if(xposition>=0 && xposition<gridwidth)
+			xposition--;
+	}
+	
+	public void moveUp(){
+		if(yposition>=0 && yposition<gridheight)
+			yposition++;
+	}
+	
+	public void moveDown(){
+		if(yposition>=0 && yposition<gridheight)
+			yposition--;
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		//add actionlistener for keystroke matching the event to keystroke
+	}
 }
