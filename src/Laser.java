0d90e645cd7bba87db64c633d93b8312dc402d75
Jake-Carducci <Jake.Carducci@gmail.com>
2015-05-12 09:00:37 -0400

    Added Laser functionality + new movement scheme
    Laser will now move across the grid in a specified direction (noted by
    axis and value) until the grid border or enemy is hit

--- src/Laser.java
+++ src/Laser.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 
@@ -11,14 +13,18 @@ public class Laser extends JComponent implements Interactable{
 	
 	private Icon lasericon;
 	private boolean laserstate;
-	private int xposition, yposition;
+	private int xposition, yposition, gridwidth, gridheight;
 	private int xdirection, ydirection;
+	private ArrayList<Interactable> list;
+	private int score;
 	private JLabel label;
 	private boolean enemy;
 	
 	public Laser(){
 		this.lasericon = new ImageIcon("C:/EclipseWorkspaces/csse220/DiggerProject/laser.png");
 		this.laserstate = true;
+		gridwidth = 700;
+		gridheight = 700;
 		this.xposition = 0;
 		this.yposition = 0;
 		label = new JLabel();
@@ -26,9 +32,11 @@ public class Laser extends JComponent implements Interactable{
 		this.enemy = true;
 	}
 	
-	public Laser(int xaxis, int yaxis){
+	public Laser(int xaxis, int yaxis, int widthofgrid, int heightofgrid){
 		this.lasericon = new ImageIcon("C:/EclipseWorkspaces/csse220/DiggerProject/laser.png");
 		this.laserstate = true;
+		this.gridwidth = widthofgrid;
+		this.gridheight = heightofgrid;
 		this.xposition = xaxis;
 		this.yposition = yaxis;
 		label = new JLabel();
@@ -55,16 +63,35 @@ public class Laser extends JComponent implements Interactable{
 	}
 	
 	// Make separate move methods?
-	public void move(){
-		//Upwards
-//		if (ydirection == 1 && yposition >= 0 && yposition < gridheight){
-//			list.set(gridwidth*yposition+xposition, new Dirt());
-//			list.get(gridwidth*yposition+xposition).transform();
-//			yposition++;
+	// dirAxis (0 is N/S, 1 is E/W) dirAmt (-1 is N, W; 1 is S, E)
+	public void move(int dirAxis, int dirAmt){
+		// Assuming the laser has not reached the border yet:
+		if (yposition-1>=0 && yposition+1<gridheight && xposition-1>=0 && xposition+1<gridwidth){
+			list.set(gridwidth*yposition+xposition, new Dirt());
+			list.get(gridwidth*yposition+xposition).transform();
+			if (dirAxis == 0) {
+				yposition=+dirAmt;
+			}
+			if (dirAxis == 1) {
+				xposition=+dirAmt;
+			}
+			Interactable temp = list.get(gridwidth*yposition+xposition);
+			if(temp.returnEnemy()){
+				this.transform();
+				return;
+			}
+			// This kills the enemy??
+			score += temp.transform();
 //			Interactable icon = list.get(gridwidth*yposition+xposition);
-//			list.set(gridwidth*yposition+xposition, this);
-//			}
-//		}
+			list.set(gridwidth*yposition+xposition, this);
+		}
+		// Once laser reaches border, it disappears.
+		else {
+			Interactable temp = list.get(gridwidth*yposition+xposition);
+			this.transform();
+			score += temp.transform();
+			return;
+		}
 	}
 	
 	public boolean returnEnemy(){
