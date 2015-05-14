1340020397f6b6eb2e3b696592ceb96a01940c8b
bartincp <bartincp@rose-hulman.edu>
2015-05-14 09:36:34 -0400

    Finished Nobbin random movement

--- src/Nobbin.java
+++ src/Nobbin.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
@@ -6,29 +8,65 @@ import javax.swing.JLabel;
 
 public class Nobbin extends JComponent implements Interactable {
 
-	protected Icon icon;
+	private Icon icon;
 	private boolean alive;
 	private int xPos, yPos;
 	private int xRespawn, yRespawn;
-	protected JLabel label;
+	private JLabel label;
 	private boolean enemy;
+	private int gridwidth;
+	private int gridheight;
+	private ArrayList<Interactable> list;
 	
-	public Nobbin(int xPos, int yPos){
+	public Nobbin(int xPos, int yPos, int widthofgrid, int heightofgrid){
 		this.xRespawn = xPos;
 		this.yRespawn = yPos;
 		this.xPos = xPos;
 		this.yPos = yPos;
+		this.gridwidth = widthofgrid;
+		this.gridheight = heightofgrid;
 		this.icon = new ImageIcon("C:/EclipseWorkspaces/csse220/DiggerProject/nobbin.png");
 		this.label = new JLabel();
 		this.label.setIcon(this.icon);
 		this.enemy = true;
 	}
 	
-	protected void moveMe(int playerX, int playerY) {
-		//
+	public void linkGrid(ArrayList<Interactable> inputlist){
+		list = inputlist;
 	}
 	
-	protected int[] getOptimalMove(int playerX, int playerY) {
+	private void moveRandom() {
+		int dir = (int)(Math.random() * 4);
+		boolean notyetmoved = true;
+		int newXPos, newYPos;
+		while (notyetmoved) {
+			if (dir == 0){
+				newYPos = yPos - 1;
+				newXPos = xPos;
+			} else if (dir == 1){
+				newYPos = yPos + 1;
+				newXPos = xPos;
+			} else if (dir == 2){
+				newYPos = yPos;
+				newXPos = xPos - 1;
+			} else {
+				newYPos = yPos;
+				newXPos = xPos + 1;
+			}
+			if (newXPos >= 0 && newXPos < gridheight && newYPos >= 0 && newYPos < gridheight) {
+				Interactable temp = list.get(gridwidth*newYPos+newXPos);
+				if (temp.getClass() == Dirt.class && !temp.returnState()){
+					xPos = newXPos;
+					yPos = newYPos;
+					list.set(gridwidth*yPos+xPos,this);
+					notyetmoved = false;
+				}
+			}
+		}
+		
+	}
+	
+	private int[] getOptimalMove(int playerX, int playerY) {
 		int dx = playerX - xPos;
 		int dy = playerY - yPos;
 		int[] move = new int[2];
@@ -59,5 +97,11 @@ public class Nobbin extends JComponent implements Interactable {
 
 	public boolean returnEnemy(){
 		return enemy;
+	}
+
+	@Override
+	public boolean returnState() {
+		// TODO Auto-generated method stub.
+		return false;
 	}
 }
