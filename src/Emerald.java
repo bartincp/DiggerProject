32632985d3a54a9b6b922722cafb55794329d792
Jake-Carducci <Jake.Carducci@gmail.com>
2015-05-22 00:51:19 -0400

    Emerald and NullSpace Javadoc
    Added public javadoc comments for the Emerald and NullSpace classes

--- src/Emerald.java
+++ src/Emerald.java
@@ -5,6 +5,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
+/**
+ * 
+ * TODO This class governs the creation and procedures of the emeralds generated on the playing space.
+ *
+ * @author Jake Carducci, Taylor Jenkins, Carl Bartine
+ *         Modified May 21, 2015.
+ */
 
 public class Emerald extends JComponent implements Interactable{
 	
@@ -15,6 +22,12 @@ public class Emerald extends JComponent implements Interactable{
 	private JLabel label;
 	private boolean enemy;
 	private static final int emeraldChange = -1;
+	
+	/**
+	 * 
+	 * TODO The constructor generates the laser sprites with associated icon, position, and state data.
+	 *
+	 */
 	
 	// Added zero-parameter block since Level had trouble constructing Emerald without it (JDC)
 	public Emerald(){
@@ -28,6 +41,12 @@ public class Emerald extends JComponent implements Interactable{
 		enemy = false;
 	}
 	
+	 /**
+	 * 
+	 * TODO The constructor generates the laser sprites with the option to specify position information.
+	 *
+	 */
+	
 	public Emerald(int xaxis, int yaxis){
 //		this.airicon = new ImageIcon("C:/EclipseWorkspaces/csse220/DiggerProject/Air.jpg");
 		this.emeraldicon = new ImageIcon("C:/EclipseWorkspaces/csse220/DiggerProject/Emerald.png");
@@ -39,6 +58,13 @@ public class Emerald extends JComponent implements Interactable{
 		enemy = false;
 	}
 
+	/**
+	 * 
+	 * TODO This method is invoked whenever the emerald is taken by a hobbin or the player
+	 *
+	 * @return The point and emerald count changes in an array
+	 */
+	
 	public int[] transform(){
 		emeraldstate = false;
 		emeraldicon = null;
@@ -50,17 +76,45 @@ public class Emerald extends JComponent implements Interactable{
 		return tempArray;
 	}
 
+	/**
+	 * 
+	 * TODO This method returns the status of the sprite as an enemy, which should be false
+	 *
+	 * @return True if an enemy, false if not
+	 */
+	
 	public boolean returnEnemy(){
 		return enemy;
 	}
+	
+	/**
+	 * 
+	 * TODO The returnIcon method returns the icon of the emerald sprite
+	 *
+	 * @return The icon of the sprite
+	 */	 
 	
 	public Icon returnIcon() {
 		return emeraldicon;
 	}
 	
+	/**
+	 * 
+	 * TODO The returnLabel method returns the JLabel of the emerald sprite
+	 *
+	 * @return The JLabel of the sprite
+	 */
+	
 	public JLabel returnLabel(){
 		return label;
 	}
+	
+	/**
+	 * 
+	 * TODO The returnState method returns the existence of the emerald sprite
+	 *
+	 * @return True if the emerald exists, false if it does not
+	 */
 	
 	public boolean returnState(){
 		return emeraldstate;
--- src/NullSpace.java
+++ src/NullSpace.java
@@ -5,15 +5,22 @@ import javax.swing.JLabel;
 
 
 /**
- * TODO Put here a description of what this class does.
+ * 
+ * TODO This class governs the creation and procedures of the null spaces in the playing space.
  *
- * @author carducjd.
- *         Created May 19, 2015.
+ * @author Jake Carducci, Taylor Jenkins, Carl Bartine
+ *         Modified May 21, 2015.
  */
 public class NullSpace extends JComponent implements Interactable{
 	private boolean nullstate;
 	private Icon nullicon;
 	private JLabel label;
+	
+	/**
+	 * 
+	 * TODO The constructor generates the null space with associated icon and state data.
+	 *
+	 */
 	
 	public NullSpace(){
 		this.nullstate=true;
@@ -22,6 +29,13 @@ public class NullSpace extends JComponent implements Interactable{
 		label.setIcon(nullicon);
 	}
 
+	/**
+	 * 
+	 * TODO This method would be invoked if the null space was being changed
+	 *
+	 * @return The point and emerald count changes in an array
+	 */
+	
 	@Override
 	public int[] transform() {
 		nullstate = false;
@@ -30,23 +44,51 @@ public class NullSpace extends JComponent implements Interactable{
 		return tempArray;
 	}
 
+	/**
+	 * 
+	 * TODO The returnIcon method returns the icon of the null space
+	 *
+	 * @return The icon of the sprite
+	 */	 
+	
 	@Override
 	public Icon returnIcon() {
 		return nullicon;
 	}
 
+	/**
+	 * 
+	 * TODO The returnLabel method returns the JLabel of the null space
+	 *
+	 * @return The JLabel of the sprite
+	 */
+	
 	@Override
 	public JLabel returnLabel() {
 		// TODO Auto-generated method stub.
 		return label;
 	}
 
+	/**
+	 * 
+	 * TODO This method returns the status of the sprite as an enemy, which should be false
+	 *
+	 * @return True if an enemy, false if not
+	 */
+	
 	@Override
 	public boolean returnEnemy() {
 		// TODO Auto-generated method stub.
 		return false;
 	}
 
+	/**
+	 * 
+	 * TODO The returnState method returns the 'existence' of the null space
+	 *
+	 * @return True if the laser exists, false if it does not
+	 */
+	
 	@Override
 	public boolean returnState() {
 		// TODO Auto-generated method stub.
